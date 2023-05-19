2f5bd62382e32cec7734f44e2b208784e23f227a
rnba12 <rnba12@yahoo.co.uk>
2023-05-19 01:32:27 +0100

    feat: add how to play

---
+++ src/components/HowTo.jsx
@@ -0,0 +1,15 @@
+function HowTo() {
+    return ( 
+        <>
+            <h1>How To Play</h1>
+            <h3>Synonymz - A word game </h3>
+            <p>Enter a word that means the same as the one shown</p>
+            <h4>Arcade</h4>
+            <p>3 Lives: Get it wrong, lose a life</p>
+            <h4>Timed</h4>
+            <p>Get as many points as you can in 1 minute</p>
+        </>
+     );
+}
+
+export default HowTo;
--- src/views/Title.jsx
+++ src/views/Title.jsx
@@ -1,20 +1,33 @@
+import { useState } from "react"
+import HowTo from "../components/HowTo"
+
 function Title({view, gameMode}) {
+    const [showHowTo, setShow] = useState(false) 
 
     function startGame(mode) {
         console.log(`Game Started in ${mode} mode`)
         gameMode(mode)
         view("game")
     }
-    function handleDocs() {
-        console.log("Show how to play")
+    function handleHowTo() {
+        setShow(prev => !prev)
     }
 
     return ( 
         <>
-        <h1>Synonymz</h1>
-        <button onClick={() => startGame("arcade")}>Arcade</button>
-        <button onClick={() => startGame("timed")}>Timed</button>
-        <button onClick={handleDocs}>How to Play</button>
+            {!showHowTo ? 
+                <>
+                    <h1>Synonymz</h1>
+                    <button onClick={() => startGame("arcade")}>Arcade</button>
+                    <button onClick={() => startGame("timed")}>Timed</button>
+                    <button onClick={handleHowTo}>How to Play</button>
+                </> 
+            :
+                <>
+                    <HowTo/>
+                    <button onClick={handleHowTo}>Back</button>
+                </>
+            }
         </>
      );
 }
