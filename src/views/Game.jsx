71d0923bc9e3a6deb8c150c8d6a1dcc116f28f7e
rnba12 <rnba12@yahoo.co.uk>
2023-05-19 12:49:55 +0100

    feat: disable form on game over

--- src/views/Game.jsx
+++ src/views/Game.jsx
@@ -5,10 +5,12 @@ function Game({ view, gameMode, setFinalScore }) {
     
     const [lives, setLives] = useState(3)
     const [score, setScore] = useState(0)
+    const [disabled, setDisabled] = useState(false)
     const [word, setWord] = useState({word: "hello", synonyms: ["hi", "greetings", "howdy"]})
     
     useEffect(() => {
         if (lives == 0) {
+            setDisabled(true)
             setFinalScore(score)
             setTimeout(() => {
             view("result")
@@ -35,13 +37,13 @@ function Game({ view, gameMode, setFinalScore }) {
 
     return ( 
         <>
-            {gameMode === "arcade" && <div className="lives">{lives} Lives Left</div>}
-            <div className="score">{score}</div>
+            {gameMode === "arcade" && <h3>{lives} Lives Left</h3>}
+            <h3>{score}</h3>
             <h2>Whats Another Word For:</h2>
             <h1>{word.word}</h1>
             <form onSubmit={handleSubmit}>
-                <input name="answer" type="text" />
-                <button>Enter</button>
+                <input name="answer" type="text" disabled={disabled} />
+                <button disabled={disabled}>Enter</button>
             </form>
         </>
     );
